9ec71dc231e8759a9114564b081da364e203e6ca
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-03-25 23:21:11 +0500

    setup webcam and canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import React , {useRef} from 'react'
-import  './App.css'
+import React, { useRef } from 'react'
+import './App.css'
 import * as tf from '@tensorflow/tfjs'
 import * as posenet from '@tensorflow-models/posenet'
 import Webcam from 'react-webcam'
 
 export default function App() {
+
+  const WebcamRef = useRef(null)
+  const canvasRef = useRef(null)
+
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <Webcam ref={WebcamRef} style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }} />
+          <canvas ref={canvasRef} style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }} />
+        </header>
+      </div>
     </>
   )
 }
